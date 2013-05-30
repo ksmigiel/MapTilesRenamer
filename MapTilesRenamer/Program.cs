ab2aafed5b2e552fa05429a8a29e08f76fb32206
Krzysztof Smigiel <smigiel.krzysztof@gmail.com>
2013-05-30 13:33:47 +0200

    Initial commit

---
+++ MapTilesRenamer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MapTilesRenamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                StringBuilder message = new StringBuilder();
+                message.AppendLine("Please use MapTilesRenamer.exe [source extension] [destination extension]");
+                message.AppendLine("and run from maps main catalog.");
+                message.AppendLine("e.g MapTilesRenamer.exe .png .png.TILE");
+                message.Append("from [puthere]\\z\\x\\y.png");
+                Console.WriteLine(message);
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Wrong parameters");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            TilesRenamer tileRenamer = new TilesRenamer(args[0], args[1]);
+            tileRenamer.RenameAll();
+            Console.WriteLine("DONE!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MapTilesRenamer/TilesRenamer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace MapTilesRenamer
+{
+    /// <summary>
+    /// Provides method for easy switching between TMS/Slippy map file naming conventions
+    /// </summary>
+    public class TilesRenamer
+    {
+        private string sourceExtension;
+        private string destExtension;
+
+        public TilesRenamer(string sourceExt, string destExt)
+        {
+            this.sourceExtension = sourceExt;
+            this.destExtension = destExt;
+        }
+
+        /// <summary>
+        /// Renames y file coordinate by calculate:
+        /// yNew = 2^zoom - yOld - 1
+        /// using \zoom\x\y.png catalog structure
+        /// </summary>
+        public void RenameAll()
+        {
+            DirectoryInfo[] zoomDirs = new DirectoryInfo(Environment.CurrentDirectory).GetDirectories();
+            foreach (DirectoryInfo zoomDir in zoomDirs)
+            {
+                int zoom = int.Parse(zoomDir.Name);
+
+                DirectoryInfo[] xDirs = zoomDir.GetDirectories();
+                foreach (DirectoryInfo xDir in xDirs)
+                {
+                    FileInfo[] yFiles = xDir.GetFiles();
+                    RenameYFiles(zoom, yFiles);
+                }
+            }
+        }
+
+        #region Private methods
+        private void RenameYFiles(int zoom, FileInfo[] yFiles)
+        {
+            foreach (FileInfo yFile in yFiles)
+            {
+                if (yFile.Name.EndsWith(sourceExtension))
+                {
+                    string newName = ComputeYCoordinate(zoom, yFile) + destExtension;
+                    File.Move(Path.Combine(yFile.DirectoryName, yFile.Name), Path.Combine(yFile.DirectoryName, newName));
+                }
+            }
+        }
+
+        private string ComputeYCoordinate(int zoom, FileInfo yFile)
+        {
+            int oldCoord = int.Parse(yFile.Name.Replace(sourceExtension, null));
+            int newCoord = (int)(Math.Pow(2, zoom) - oldCoord - 1);
+            return newCoord.ToString();
+        }
+        #endregion
+    }
+}
